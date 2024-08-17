e6a6dd769cdeb50e0e27c0bd02a354aa50881bbf
“nandakyawwin” <nandakyawwin@globalwave.company>
2024-08-17 23:53:14 +0630

    routing & menu

--- src/app/demo/components/admin/admin-routing.module.ts
+++ src/app/demo/components/admin/admin-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'users',  data: { breadcrumb: 'Button' }, loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'roles',  data: { breadcrumb: 'Button' }, loadChildren: () => import('./role/role.module').then(m => m.RoleModule) },
+  { path: 'users', data: { breadcrumb: 'Button' }, loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  { path: 'roles', data: { breadcrumb: 'Button' }, loadChildren: () => import('./role/role.module').then(m => m.RoleModule) },
   { path: 'systems', data: { breadcrumb: 'Button' }, loadChildren: () => import('./system/system.module').then(m => m.SystemModule) },
   { path: 'languages', data: { breadcrumb: 'Button' }, loadChildren: () => import('./language/language.module').then(m => m.LanguageModule) },
-  { path: 'tables',  data: { breadcrumb: 'Button' }, loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
-  { path: 'filters',  data: { breadcrumb: 'Button' }, loadChildren: () => import('./filter/filter.module').then(m => m.FilterModule) },
+  { path: 'tables', data: { breadcrumb: 'Button' }, loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
+  { path: 'filters', data: { breadcrumb: 'Button' }, loadChildren: () => import('./filter/filter.module').then(m => m.FilterModule) },
+  { path: 'lists', data: { breadcrumb: 'Button' }, loadChildren: () => import('./table List/list.module').then(m => m.ListModule) },
+  { path: 'ann', data: { breadcrumb: 'Button' }, loadChildren: () => import('./ann/ann.module').then(m => m.AnnModule) },
   { path: '**', redirectTo: '/notfound' }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
