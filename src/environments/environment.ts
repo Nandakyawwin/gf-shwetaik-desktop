9b6d0bd442523662a49c664959fdccfaf54bd6af
Nandakyawwin <nandakyawwin@globalwave.company>
2024-09-24 09:24:49 +0630

    change url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   encryptionKey: 'GOLD_FUTURE$^&*@',
-  BASEURL: `http://localhost:3000/admin/superuser/` // Internal Testing
-  // BASEURL : `https://st.golden-future.org/admin/superuser/` // Production
+  // BASEURL: `http://localhost:3000/admin/superuser/` // Internal Testing
+  BASEURL: `https://st.golden-future.org/admin/superuser/` // Production
 };
